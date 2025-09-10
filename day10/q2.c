f06929eb3e3b5937f5f00dab2efcc00eb8d895b6
harshit8451 <2711harshitchoudhary@gmail.com>
2025-09-10 15:11:35 +0530

    done

--- day10/q2.c
+++ day10/q2.c
@@ -14,3 +14,41 @@ Friday
 
 */
 
+
+#include<stdio.h>
+
+int main(void)
+{
+    int num;
+
+    printf("Enter number from 1 to 7: ");
+    scanf("%d", &num);
+
+    switch (num)
+    {
+        case 1 :
+            printf("Monday");
+            break;
+        case 2 :
+            printf("Tuesday");
+            break;
+        case 3 :
+            printf("Wednesday");
+            break;
+        case 4 :
+            printf("Thursday");
+            break;
+        case 5 :
+            printf("Friday");
+            break;
+        case 6 :
+            printf("Saturday");
+            break;
+        case 7 :
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid Input!");
+            break;
+    }
+}
