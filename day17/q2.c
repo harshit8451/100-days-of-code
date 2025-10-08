9fc44197cfb37d22f86b4145b230e4732eac38db
harshit8451 <2711harshitchoudhary@gmail.com>
2025-10-09 05:26:01 +0530

    done

--- day17/q2.c
+++ day17/q2.c
@@ -13,3 +13,30 @@ Output 2:
 Not prime
 
 */
+
+#include<stdio.h>
+
+int is_prime(int a)
+{
+    for(int i = 2; i < a/2; i++)
+    {
+        if(a % i == 0)
+        {
+            printf("Not prime..");
+            return 0;
+        }
+    }
+    printf("Prime number!!");
+}
+
+
+int main(void)
+{
+    int input;
+
+    printf("Enter a number: ");
+    scanf("%d", &input);
+    is_prime(input);
+
+    return 0;
+}
